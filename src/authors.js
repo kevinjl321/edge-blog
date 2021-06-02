4cd3cefb0bf90c6f30d1114436e06e7413351558
Kevin Lan <kevinlan321@gmail.com>
2021-06-02 16:22:41 -0700

    first commit + test post

---
+++ src/authors.js
@@ -0,0 +1,7 @@
+import kevinlanAvatar from './img/kevinlan.jpg'
+
+export const kevinlan = {
+  name: 'Kevin Lan',
+  twitter: 'kevinjl321',
+  avatar: kevinlanAvatar,
+}
---
+++ src/components/Header.js
@@ -0,0 +1,26 @@
+import Link from 'next/link'
+
+export default function Header() {
+  return (
+    <header className="flex justify-between items-center py-10">
+      <div>
+        <Link href="/">
+          <a 
+            aria-label="Project Edge Blog"
+            className="text-4xl font-bold text-gray-700 hover:text-green-500"
+          >
+            project edge blog ⚛️
+          </a>
+        </Link>
+      </div>
+      <div className="text-base leading-5">
+        <a
+          href="https://project-edge.org"
+          className="font-medium text-gray-500 hover:text-gray-700"
+        >
+          Main website &rarr;
+        </a>
+      </div>
+    </header>
+  )
+}
