38e57aa41869ce82ad3d7e51174cd16252fefb9d
Kevin Lan <kevinlan321@gmail.com>
2021-06-09 01:00:33 -0700

    add authors list + change 1st article author

--- src/authors.js
+++ src/authors.js
@@ -1,7 +1,49 @@
 import kevinlanAvatar from './img/kevinlan.jpg'
+import dowsonyangAvatar from './img/dowsonyang.jpg'
+import lawrencechaoAvatar from './img/lawrencechao.jpg'
+import markmacAvatar from './img/markmac.jpg'
+import randycaiAvatar from './img/randycai.jpg'
+import coreysyAvatar from './img/coreysy.jpg'
+import sriniAvatar from './img/srini.jpg'
 
 export const kevinlan = {
   name: 'Kevin Lan',
   twitter: 'kevinjl321',
   avatar: kevinlanAvatar,
 }
+export const markmac = {
+  name: 'Mark MacDermott',
+  twitter:'',
+  avatar: markmacAvatar,
+}
+export const coreysy = {
+  name: 'Corey Sy',
+  twitter: '',
+  avatar: coreysyAvatar,
+}
+export const srini = {
+  name: 'Srinivasan Arumugham',
+  twitter: '',
+  avatar: sriniAvatar,
+}
+export const kevinlan = {
+  name: 'Kevin Lan',
+  twitter: 'kevinjl321',
+  avatar: kevinlanAvatar,
+}
+export const randycai = {
+  name: 'Randy Cai',
+  twitter: '',
+  avatar: randycaiAvatar,
+}
+export const lawrencechao = {
+  name: 'Lawrence Chao',
+  twitter: '',
+  avatar: lawrencechaoAvatar,
+}
+export const dowsonyang = {
+  name: 'Dowson Yang',
+  twitter: '',
+  avatar: dowsonyangAvatar,
+}
+
